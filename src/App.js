63dd3ad19175c2b94b02a4e81e4770d5dd73e362
S Mugenzi <smugenzi@Ss-MacBook-Air.local>
2020-06-18 13:01:48 -0400

    Changed title, removed serverless function

--- src/App.js
+++ src/App.js
@@ -3,18 +3,9 @@ import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
-  const [date, setDate] = useState(null);
-  useEffect(() => {
-    async function getDate() {
-      const res = await fetch('/api/date');
-      const newDate = await res.text();
-      setDate(newDate);
-    }
-    getDate();
-  }, []);
   return (
     <main>
-      <h1>Create React App + Go API</h1>
+      <h1>Welcome to Vercel previously Zeit!</h1>
       <h2>
         Deployed with{' '}
         <a
@@ -49,7 +40,6 @@ function App() {
       </p>
       <br />
       <h2>The date according to Go is:</h2>
-      <p>{date ? date : 'Loading date...'}</p>
     </main>
   );
 }
